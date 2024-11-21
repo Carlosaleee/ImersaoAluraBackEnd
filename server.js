94164d39bfb9b364a1146a44e3fd374d392a71c1
“Carlos <alexandregularte013@gmail.com>
2024-11-20 23:12:15 -0300

    Atualizações referente a Aula 2

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "http://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Gato brincando com uma bola de pelo.",
+        imagem: "http://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Gato brincando com um novelo de lã",
+        imagem: "http://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("BOas Vindas á Imersão!")
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
+function buscarPostPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
 
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+});
 
