7408b552e5e4050bfa49a75d2f57df4c5f40a036
“Carlos <alexandregularte013@gmail.com>
2024-11-26 14:33:32 -0300

    Imersão Alura Back End - Aula 4

--- src/model/postsModel.js
+++ src/model/postsModel.js
@@ -1,23 +1,23 @@
-import conectarAoBanco from "../config/dbConfig.js";
-    
-    // Conecta ao banco de dados utilizando a string de conexão fornecida pela variável de ambiente STRING_CONEXAO
-    const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+import conectarAoBanco from "../config/dbConfig.js"
 
-    // Função assíncrona para obter todos os posts do banco de dados
-    export async function getTodosPosts() {
+// Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-    // Seleciona o banco de dados "ImersaoAlura"
+// Função assíncrona para buscar todos os posts do banco de dados
+export async function getTodosPosts() {
+   
+    // Seleciona o banco de dados "imersao-instabytes"
     const db = conexao.db("ImersaoAlura");
-
-    // Seleciona a coleção "posts"
+    
+    // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
-
+    
     // Retorna um array com todos os documentos da coleção
     return colecao.find().toArray();
 }
 
-    export async function criarPost(novoPost){
-
+export async function criarPost(novoPost) {
+    
     const db = conexao.db("ImersaoAlura");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost);
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,13 +1,29 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controller/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controller/postsController.js";
 
-const router = express.Router();
+// Configuração do multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Define a pasta para salvar os arquivos
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Mantém o nome original do arquivo
+  },
+});
+
+const upload = multer({ storage }); // Middleware para upload
+
+const router = express.Router(); // Criação do router
 
 // Rota para obter todos os posts
-router.get("/posts", listarPosts);
+router.get("/", listarPosts);
 
 // Rota para criar um novo post
 router.post("/", postarNovoPost);
 
-// Exporta o objeto Router
+// Rota para upload de imagens
+router.post("/upload", upload.single("imagem"), uploadImagem);
+
+// Exporta o objeto router
 export default router;
